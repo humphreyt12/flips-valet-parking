b7036f130aecc752f3ed6ef0cd571aefa10e0324
Tina Humphrey <humphrey14t@gmail.com>
2024-04-16 23:11:27 -0400

    commit changes to App.jsx, app.scss, index.scsss and added Services.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,19 @@
-import './app.css';
+// Importing app.css and the required sections for the app
+import './app.scss';
 import Navbar from './components/Navbar';
 import Contact from './components/Contact';
 import About from './components/About';
+import Services from './components/Services';
 
 function App() {
   return (
-    <div className="homepage">
-      <Navbar />
-      <Contact />
-      <About />
+    <div> 
+      <section id ="Homepage">
+         <Navbar />
+         <Services />
+      </section>
+      <section id="About Our Company"><About /></section> 
+      <section id="Contact"><Contact/></section>
     </div>
   );
 }

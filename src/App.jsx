7b7cbf2cbfd0dc0e5cd130efd791652ec49a2102
Tina Humphrey <humphrey14t@gmail.com>
2024-05-12 01:10:59 -0400

    commit changes to home.jsx and app.jsx to add the carousel and mobile responsiveness

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,26 @@
 // Importing app.css, the components and react-router-dom dependency
 import "./app.scss";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Media from 'react-media';
+import ThemeProvider from 'react-bootstrap/ThemeProvider'
 import Navbar from "./components/navbar/Navbar";
 import Footer from "./components/footer/Footer";
-// import Services from './components/services/Services';
 
+// Importing the pages
 import Home from "./pages/home/Home";
 import About from "./pages/about/About";
 import Contact from "./pages/contact/Contact";
-// import Contact from './components/contact/Contact';
+
 function App() {
   return (
     <div className="appContainer">
+      {/* Media Inquiries for Differnt Size Screens */}
+      <ThemeProvider 
+      breakpoints={['xxxl', 'xxl', 'xl', 'lg', 'md', 'sm', 'xs', 'xxs']}
+      minBreakpoint="xxs"
+      >
+      <Media query="(min-width: 600px">
+        {/*Browser Router allows the app to navigate through the pages  */}
       <BrowserRouter>
         <Navbar />
         <Routes>
@@ -19,10 +28,13 @@ function App() {
           <Route path="/about" element={<About />} />
           <Route path="/contact" element={<Contact />} />
         </Routes>
+        {/* The Footer */}
         <Footer />
       </BrowserRouter>
+      </Media>
+      </ThemeProvider>
     </div>
   );
-}
+};
 
 export default App;
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,10 +1,8 @@
 //Importing the Hompage
 import "./home.scss";
+import Carousel from "react-bootstrap/Carousel";
 import Navbar from "../../components/navbar/Navbar";
 import Services from "../../components/services/Services";
-
-// Import React
-// import React, { useState } from 'react';
 
 const Home = () => {
   return (
@@ -12,8 +10,22 @@ const Home = () => {
       <Navbar />
       <div className="textContainer">
         <h1>Flips Valet Parking</h1>
+      <Carousel>
+        <Carousel.Item>
         <img src="/hero.png" alt="Hero Image" />
-     
+        </Carousel.Item>
+      <Carousel.Item>
+       <img src="/photo2.JPG" alt="photo2" />
+      </Carousel.Item>
+      <Carousel.Item>
+      <img src="/photo4.JPG" alt="photo4" />
+    
+      </Carousel.Item>
+      <Carousel.Item>
+      <img src="/photo5.jpg" alt="photo5" />
+       
+      </Carousel.Item>
+      </Carousel>
       </div>
       <section id="services">
           <Services />
